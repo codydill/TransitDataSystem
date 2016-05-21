5f2e7f54e80fde718bf2799e67f12bbb4e52de9a
Cody Dillon <codywdillongithub@gmail.com>
2016-05-21 08:13:52 -0700

    Removed OnBoard check constraints.

--- TransitDataSystem/TransitDatabase/Bus/Tables/OnBoard.sql
+++ TransitDataSystem/TransitDatabase/Bus/Tables/OnBoard.sql
@@ -9,10 +9,6 @@
     [HandicapOnBoard] INT              NULL,
     CONSTRAINT [PK_Stop] PRIMARY KEY CLUSTERED ([StopID] ASC),
     CONSTRAINT [FK_Bus] FOREIGN KEY ([BusID]) REFERENCES [Bus].[RouteDetails] ([BusID]),
-    CONSTRAINT [FK_Location] FOREIGN KEY ([LocationID]) REFERENCES [Bus].[LocationDetails] ([LocationID]), 
-    CONSTRAINT [CK_Youth] CHECK (YouthOnBoard >= 0), 
-    CONSTRAINT [CK_Adult] CHECK (AdultOnBoard >= 0), 
-    CONSTRAINT [CK_Senior] CHECK (SeniorOnBoard >= 0), 
-    CONSTRAINT [CK_Handicap] CHECK (HandicapOnBoard >= 0)
+    CONSTRAINT [FK_Location] FOREIGN KEY ([LocationID]) REFERENCES [Bus].[LocationDetails] ([LocationID]) 
 );
 
