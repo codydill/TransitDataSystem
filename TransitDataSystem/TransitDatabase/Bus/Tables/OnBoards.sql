192239a7ba5e83e786bebff18002f3289f32232f
Cody Dillon <codywdillongithub@gmail.com>
2016-06-07 03:05:34 -0700

    Added Tags Table, configured OnBoardDetails.

--- TransitDataSystem/TransitDatabase/Bus/Tables/OnBoards.sql
+++ TransitDataSystem/TransitDatabase/Bus/Tables/OnBoards.sql
@@ -1,12 +1,8 @@
-﻿CREATE TABLE [Bus].[OnBoard] (
+﻿CREATE TABLE [Bus].[OnBoards] (
     [OnBoardID]          INT  NOT NULL,
     [LocationID]      INT NOT NULL,
     [BusID]           INT NOT NULL,
     [OnBoardTimeStamp]   DATETIME2 (7)    NOT NULL,
-    [YouthOnBoard]    TINYINT              NULL,
-    [AdultOnBoard]    TINYINT              NULL,
-    [SeniorOnBoard]   TINYINT              NULL,
-    [HandicapOnBoard] TINYINT              NULL,
     CONSTRAINT [PK_OnBoard] PRIMARY KEY CLUSTERED ([OnBoardID] ASC),
     CONSTRAINT [FK_Bus_ToBuses] FOREIGN KEY ([BusID]) REFERENCES [Bus].[Buses] ([BusID]),
     CONSTRAINT [FK_Location_ToLocations] FOREIGN KEY ([LocationID]) REFERENCES [Bus].[Locations] ([LocationID]) 
--- TransitDataSystem/TransitDatabase/Bus/Tables/SpecialOnBoard.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [Bus].[SpecialOnBoard] (
-    [SpecialID]    INT  NOT NULL,
-    [OnBoardID]       INT NOT NULL,
-    [SpecialTag]   VARCHAR (75)     NOT NULL,
-    [SpecialCount] TINYINT              NULL,
-    CONSTRAINT [PK_Special] PRIMARY KEY CLUSTERED ([SpecialID] ASC),
-    CONSTRAINT [FK_OnBoardID_ToOnBoard] FOREIGN KEY ([OnBoardID]) REFERENCES [Bus].[OnBoard] ([OnBoardID])
-);
-
