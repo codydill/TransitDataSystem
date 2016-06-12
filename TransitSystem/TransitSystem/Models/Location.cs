ccfd45496cd3bd4cfc7cf49f67a3258f04283019
Cody Dillon <codywdillongithub@gmail.com>
2016-06-12 04:02:20 -0700

    Removed date fields. Added bool for is tag current.

--- TransitSystem/TransitSystem/Models/Location.cs
+++ TransitSystem/TransitSystem/Models/Location.cs
@@ -20,10 +20,10 @@ namespace TransitSystem.Models
         public int LocationID { get; set; }
 
         [StringLength(75)]
-        public string Address { get; set; }
+        public string Name { get; set; }
 
         [StringLength(75)]
-        public string Name { get; set; }
+        public string Address { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OnBoard> OnBoards { get; set; }
--- TransitSystem/TransitSystem/Models/Tag.cs
+++ TransitSystem/TransitSystem/Models/Tag.cs
@@ -22,11 +22,7 @@ namespace TransitSystem.Models
         [StringLength(75)]
         public string Description { get; set; }
 
-        [Column(TypeName = "datetime2")]
-        public DateTime? BeginDate { get; set; }
-
-        [Column(TypeName = "datetime2")]
-        public DateTime? EndDate { get; set; }
+        public bool Current { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OnBoardDetail> OnBoardDetail { get; set; }
