7d1ee25c36968e5cbac3df08d4d411028e11d90c
Cody Dillon <codywdillongithub@gmail.com>
2016-06-22 14:52:18 -0700

    Creates test data for OnBoards and OnBoardDetails.

--- TransitSystem/TransitSystem/Migrations/Configuration.cs
+++ TransitSystem/TransitSystem/Migrations/Configuration.cs
@@ -21,6 +21,8 @@ namespace TransitSystem.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
+
+            //Creating Tags
             var tags = new List<Tag>
             {
                 new Tag { Description = "Adult", Current = true },
@@ -30,6 +32,8 @@ namespace TransitSystem.Migrations
             tags.ForEach(t => context.Tags.AddOrUpdate(d => d.Description, t));
             context.SaveChanges();
 
+
+            //Creating Locations, Routes, and RouteDetails
             var locations = new List<Location>
             {
                 new Location { Name = "Transit Station", Address = "North 6th Avenue" },
@@ -45,6 +49,7 @@ namespace TransitSystem.Migrations
             locations.ForEach(l => context.Locations.AddOrUpdate(d => d.Name, l));
             context.SaveChanges();
 
+
             var foodRoute = new Route { RouteName = "Food Route" };
             var bankRoute = new Route { RouteName = "Bank Route" };
             var medicalRoute = new Route { RouteName = "Medical Route" };
@@ -52,16 +57,22 @@ namespace TransitSystem.Migrations
             routes.ForEach(r => context.Routes.AddOrUpdate(d => d.RouteName, r));
             context.SaveChanges();
 
+
+            var station = context.Locations.Single(l => l.Name.Contains("Station"));
+
             var foodLocations = locations.FindAll(l => l.Name.Contains("Food"));
+            foodLocations.Insert(0, station);
             AddLocationsToRoute(context, foodRoute, foodLocations);
             context.SaveChanges();
 
             var bankLocations = locations.FindAll(l => l.Name.Contains("Bank"));
+            bankLocations.Insert(0, station);
             AddLocationsToRoute(context, bankRoute, bankLocations);
             context.SaveChanges();
 
-
-
+            //Creating OnBoards
+            CreateOnBoardsForAllRoutes(context, DateTime.Today, 5);
+            context.SaveChanges();
 
 
 
@@ -80,23 +91,38 @@ namespace TransitSystem.Migrations
                 context.RouteDetails.AddOrUpdate(new RouteDetail { RouteID = route.RouteID, LocationID = loc.LocationID, Position = i++ });
             }
         }
-        //public void AddLocations(IEnumerable<Location> locations)
-        //{
-        //    byte i = 0;
-        //    foreach (var loc in locations)
-        //    {
-        //        RouteDetails.Add(new RouteDetail { RouteID = RouteID, LocationID = loc.LocationID, Position = i++ });
-        //    }
-        //}
 
-        //public void RemoveLocations()
-        //{
-        //    RouteDetail[] details = new RouteDetail[RouteDetails.Count];
-        //    RouteDetails.CopyTo(details, 0);
-        //    foreach (var detail in details)
-        //    {
-        //        RouteDetails.Remove(detail);
-        //    }
-        //}
+
+        public OnBoard CreateOnBoard(TransitSystem.DAL.TransitContext context, Location loc, Route route, DateTime date)
+        {
+            var onBoard = new OnBoard { Location = loc, Route = route, OnBoardTimeStamp = date };
+            context.OnBoards.Add(onBoard);
+            return onBoard;
+        }
+        public void CreateOnBoardDetails(TransitSystem.DAL.TransitContext context, List<Tag> tags, OnBoard onBoard, Random rand)
+        {
+            foreach (var tag in tags)
+            {
+                context.OnBoardDetails.Add(new OnBoardDetail { Tag = tag, OnBoard = onBoard, Count = (byte)rand.Next(1, 2) });
+            }
+        }
+        public void CreateOnBoardsForAllRoutes(TransitSystem.DAL.TransitContext context, DateTime date, double minutesBetweenLocations)
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+            List<Tag> currentTags = context.Tags.Where(t => t.Current == true).ToList();
+            List<Route> allRoutes = context.Routes.ToList();
+            foreach (var route in allRoutes)
+            {
+                IEnumerable<Location> locations = route.RouteDetails.Select(r => r.Location);
+                DateTime dateIncremented = date;
+                foreach (var loc in locations)
+                {
+                    dateIncremented = dateIncremented.AddMinutes(minutesBetweenLocations);
+                    var onBoard = CreateOnBoard(context, loc, route, dateIncremented);
+                    CreateOnBoardDetails(context, currentTags, onBoard, rand);
+                }
+
+            }
+        }
     }
 }
