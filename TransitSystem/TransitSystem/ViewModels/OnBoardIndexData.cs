9b9d93fbe1de5e3a1efa97a6e1568d64d56eb63b
Cody Dillon <codywdillongithub@gmail.com>
2016-06-24 06:02:37 -0700

    Added viewmodel for OnBoard controller.

---
+++ TransitSystem/TransitSystem/ViewModels/OnBoardIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TransitSystem.Models;
+
+namespace TransitSystem.ViewModels
+{
+    public class OnBoardIndexData
+    {
+        public IEnumerable<Route> Routes { get; set; }
+        public IEnumerable<Tag> CurrentTags { get; set; }
+    }
+}
