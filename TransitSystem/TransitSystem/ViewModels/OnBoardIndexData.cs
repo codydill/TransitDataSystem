8bc9c7f4dcb47a48405b914543d26b07f253d3f1
Cody Dillon <codywdillongithub@gmail.com>
2016-06-24 06:20:01 -0700

    Added location collection property to OnBoard viewmodel.

--- TransitSystem/TransitSystem/ViewModels/OnBoardIndexData.cs
+++ TransitSystem/TransitSystem/ViewModels/OnBoardIndexData.cs
@@ -10,5 +10,6 @@ namespace TransitSystem.ViewModels
     {
         public IEnumerable<Route> Routes { get; set; }
         public IEnumerable<Tag> CurrentTags { get; set; }
+        public IEnumerable<Location> RouteLocations { get; set; }
     }
 }
