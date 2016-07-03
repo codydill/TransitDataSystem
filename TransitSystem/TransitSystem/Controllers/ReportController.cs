82eba25c40b0e5b47fdbbfaed7360af945f3e2e3
Cody Dillon <codywdillongithub@gmail.com>
2016-07-03 04:50:19 -0700

    Revert "Should not be a commit"
    This reverts commit f1a9b1088e8974a8f8fed57f424acf826bc2e0a5.

--- TransitSystem/TransitSystem/Controllers/ReportController.cs
+++ TransitSystem/TransitSystem/Controllers/ReportController.cs
@@ -11,7 +11,7 @@ namespace TransitSystem.Controllers
         // GET: Report
         public ActionResult Index()
         {
-            return Redirect("");
+            return Redirect("Enter Web Portal URL Address Here.");
         }
     }
 }
